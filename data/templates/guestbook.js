5f08c79601743f338ce2d831192b6ac68abd8a12
eqj <jenndar@gmail.com>
2017-08-16 17:49:43 -0700

    Make it a bit more 90s

--- data/templates/guestbook.js
+++ data/templates/guestbook.js
@@ -4,7 +4,7 @@ const functionThatConvertsAMessageIntoHtml = (post) => {
     return `
     <p>
     ${escape(post['message'])}<br>
-    -- <i>${escape(post['name'])}, ${escape(post['createdAt'])}</i>
+    -- <i>${escape(post['name'])}, ${escape(post['createdAt'].toDateString())}</i>
     </p>
     `;
 };
@@ -20,39 +20,40 @@ module.exports = (posts, visits, alerts) => {
 
     return `<!DOCTYPE html>
 <html lang="en">
-<head>
-    <title>Leave a message</title>
-    <link rel="stylesheet" type="text/css" href="public/style.css">
-    ${ alerts.map(x => functionThatConvertsAlertIntoHtml(x)).join("") }
-</head>
-<body>
-    <div id="main">
-       <h1>Guestbook like it's 1999</h1>
-       <form id="contact_form" action="#" method="POST" enctype="application/x-www-form-urlencoded">
-        <div class="row">
-            <label for="name">Your name:</label><br />
-            <input id="name" class="input" name="name" type="text" value="" size="30" /><br />
+    <head>
+        <title>Leave a message</title>
+        <link rel="stylesheet" type="text/css" href="public/gueststyle.css">
+        ${ alerts.map(x => functionThatConvertsAlertIntoHtml(x)).join("") }
+    </head>
+    <body>
+        <div id="main">
+            <div id="bar">
+                <h1>Guestbook like it's 1999!</h1>
+            </div>
+            <form id="contact_form" action="#" method="POST" enctype="application/x-www-form-urlencoded">
+                <div class="row">
+                    <label for="name">Your name:</label><br />
+                    <input id="name" class="input" name="name" type="text" value="" size="30" /><br />
+                </div>
+                <div class="row">
+                    <label for="email">Your email:</label><br />
+                    <input id="email" class="input" name="email" type="text" value="" size="30" /><br />
+                </div>
+                <div class="row">
+                    <label for="message">Your message:</label><br />
+                    <textarea id="message" class="input" name="message" rows="7" cols="30"></textarea><br />
+                </div>
+                    <input id="submit_button" type="submit" value="Post message" />
+            </form>
+            <img id="header" src="public/images/kilroy.jpg">
+            <div id="content">
+                ${ posts.map(x => functionThatConvertsAMessageIntoHtml(x)).join("") }
+            </div>
+            <div id="bar">
+                <h2>Visitors: ${visits}</h2>
+            </div>
         </div>
-        <div class="row">
-            <label for="email">Your email:</label><br />
-            <input id="email" class="input" name="email" type="text" value="" size="30" /><br />
-        </div>
-        <div class="row">
-            <label for="message">Your message:</label><br />
-            <textarea id="message" class="input" name="message" rows="7" cols="30"></textarea><br />
-        </div>
-        <input id="submit_button" type="submit" value="Post message" />
-        </form>
-        
-        <img id="header" src="public/kilroy.jpg">
-        <div id="content">
-            ${ posts.map(x => functionThatConvertsAMessageIntoHtml(x)).join("") }
-        </div>
-        <div id="footer">
-            <h2>Visitors: ${visits}</h2>
-        </div>
-    </div>
-</body>
+    </body>
 </html>
 `;
 };
